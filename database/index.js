528118278e4ea2df90ac0a104aa27aa317aecd9a
Amy <amy@amogus.cloud>
2024-08-29 14:06:28 -0400

    chore: update lastActive based on last message
    TODO: identify spam

--- database/index.js
+++ database/index.js
@@ -11,4 +11,17 @@ const knex = Knex({
 	}
 });
 
-export { knex }
+/**
+ * @typedef {Object} ServerActivity
+ * @prop {string} user - ID of the user
+ * @prop {number} lastActive - Timestamp
+ */
+
+/**
+ * @function
+ * @prop {string} id - Server id
+ * @returns {Knex.QueryBuilder<ServerActivity, {}>}
+ */
+const Server = (id) => knex(id)
+
+export { knex, Server }
--- events/messageSend.js
+++ events/messageSend.js
@@ -1,9 +1,26 @@
+import { Server } from "../database/index.js"
 /**
  * @param {import("revolt.js").Message} message
  */
-function messageSend(message) {
+async function messageSend(message) {
 	console.log("New message!")
-	if (message.author.bot || message.systemMessage || !message.author) return // Exclude bot users from the action
+	if (message.author.bot || !message.server || message.systemMessage || !message.author) return // Exclude bot users from the action
+
+	// check if the user is already in the database
+	const user = await Server(message.server.id).where('user', message.author.id).first("user", "lastActive")
+
+	if (!user) {
+		await Server(message.server.id).insert({
+			user: message.author.id,
+			lastActive: message.createdAt
+		})
+	} else {
+		console.log(user)
+		await Server(message.server.id).where("user", message.author.id).update({
+			lastActive: message.createdAt
+		})
+
+	}
 }
 
 export { messageSend } 
