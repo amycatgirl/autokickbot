567c6bbde1ffd6cf0174f59bdaffcb4c16736a97
Amy <amy@amogus.cloud>
2024-08-29 17:55:02 -0400

    fix: actually commit the changes
    i swear i did that before

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import "dotenv/config"
-import "./database/index.js"
 
 import { Client } from "revolt.js"
 import { CommandHandler } from "./commands/index.js"
 import { guildJoin, guildLeave, messageSend } from "./events/index.js"
+import { Server } from "./database/index.js"
 
 const client = new Client()
 const commandHandler = new CommandHandler()
@@ -13,6 +13,28 @@ client.once("ready", () => {
 	console.log("Ready!")
 	console.info(`Logged in as ${client.user.username}. Watching ${client.servers.size()} servers.`)
 
+	// TODO decouple logic
+	setTimeout(async () => {
+		for (const server of client.servers.values()) {
+			const inactiveUsers = await Server(server.id)
+				.where('lastActive', '<', knex.raw('NOW() - INTERVAL \'1 week\''))
+				.select('user');
+			if (!inactiveUsers) continue;
+
+			for (const user of inactiveUsers) {
+				const dms = await client.users.get(user).openDM()
+				await dms.sendMessage({
+					embeds: [
+						{
+							title: "AK - Kicked for inactivity",
+							description: "You have been kicked for inactivity. If you wish to continue being inside the server, rejoin it via discover."
+						}
+					]
+				})
+				await server.kickUser(user)
+			}
+		}
+	}, 60000)
 })
 
 // TODO make this a bit cleaner maybe?
