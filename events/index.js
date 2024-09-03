87af989e41a806968beb815d4e620200a1fcb620
Amy <amy@amogus.cloud>
2024-09-03 09:40:46 -0400

    fix: actually export syncConfig

--- events/index.js
+++ events/index.js
@@ -2,5 +2,6 @@ import { guildJoin } from "./guildJoin.js"
 import { guildLeave } from "./guildLeave.js"
 import { messageSend } from "./messageSend.js"
 import { thisisthepartwherehekillsyou } from "./thepartwherehekillsyou.js"
+import { syncConfig } from "./syncConfig.js"
 
-export { guildJoin, guildLeave, messageSend, thisisthepartwherehekillsyou }
+export { guildJoin, guildLeave, messageSend, thisisthepartwherehekillsyou, syncConfig }
