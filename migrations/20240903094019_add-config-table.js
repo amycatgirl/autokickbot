b7d009224dac2cdd3e1e0c188c94315b32309086
Amy <amy@amogus.cloud>
2024-10-06 20:32:00 -0400

    feat(migrations): add config table migration

---
+++ migrations/20240903094019_add-config-table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable("config", (table) => {
+	table.string("server", 26).primary()
+	table.string("maxInactivePeriod") 
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable("config")
+};
