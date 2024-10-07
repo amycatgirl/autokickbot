88f817eacb26a21e6b4364dcd61cecb913e851eb
Amy <amy@amogus.cloud>
2024-10-06 20:31:14 -0400

    chore: fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,8 @@ export default {
   production: {
     client: 'postgresql',
     connection: {
+      host: "db",
+      port: 5432,
       database: process.env.POSTGRES_DB,
       user:     process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD
