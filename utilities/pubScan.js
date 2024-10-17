2b05f45e539108a5495f37277f43a88b282065f6
Amy <amy@amogus.cloud>
2024-10-17 18:11:48 -0400

    fix: migrate getKeys to scanStream

--- utilities/pubScan.js
+++ utilities/pubScan.js
@@ -1,24 +1,42 @@
-import { pub } from "../database/redis.js"
+// @ts-check
+import { pub } from "../database/redis.js";
 
 /**
  * @async
  *
  * Get keys from Redis pub
- * 
+ *
  * @param {string} [pattern="*"] - Pattern to match
  * @param {number} [count=10]
  * @returns {Promise<any>}
  */
-async function getKeys(pattern="*", count=10) {
+async function getKeys(pattern = "*", count = 10) {
+  /** @type {string[]} */
+  const result = await new Promise((resolve, reject) => {
+    /** @type {string[]} */
     const results = [];
-    const iteratorParams = {
-        MATCH: pattern,
-        COUNT: count
-    }
-    for await (const key of pub.scanIterator(iteratorParams)) {
+
+    const stream = pub.scanStream({
+      match: pattern,
+      count,
+    });
+
+    stream.on("data", (results) => {
+      for (const key in results) {
         results.push(key);
-    }
-    return results;
+      }
+    });
+
+    stream.on("end", () => {
+      resolve(results);
+    });
+
+    stream.on("error", (error) => {
+      reject(error);
+    });
+  });
+
+  return result;
 }
 
-export { getKeys }
+export { getKeys };
